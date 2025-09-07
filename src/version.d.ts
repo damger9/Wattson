d843f7b6b388de87a45d7672906cfe0b3178a3e8
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-09-07 16:40:00 +0200

    Create version.d.ts

---
+++ src/version.d.ts
@@ -0,0 +1,4 @@
+// File that will be created in /build by the version script - REQUIRES THE GIT REPO TO WORK!
+// UPLOAD /build to the server running the code; do NOT build it on the server.
+export const version: string;
+export const commit_id: string;
