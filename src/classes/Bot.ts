30ee6a0f0bb318cacfdbb3535196fdcb719a1f97
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-09-08 08:55:54 +0200

    ...

---
+++ src/classes/Bot.ts
@@ -0,0 +1,16 @@
+import { Client, ClientOptions } from "discord.js";
+
+import { informativeLogging, developmentMode } from "../config.json";
+
+export class Bot extends Client {
+    public readonly InformativeLogging: boolean;
+    public readonly DeveloperMode: boolean;
+
+
+    constructor(clientOptions: ClientOptions) {
+        super(clientOptions)
+
+        this.InformativeLogging = informativeLogging;
+        this.DeveloperMode = developmentMode;
+    }
+}
